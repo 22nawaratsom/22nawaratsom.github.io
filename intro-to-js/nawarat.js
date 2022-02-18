708588fae6e8ee28fde5a390bcf813c7552069b4
Nawarat Wattanathaworn <nawanin86@gmail.com>
2022-02-18 14:01:04 +0700

    ...

--- intro-to-js/nawarat.js
+++ intro-to-js/nawarat.js
@@ -9,3 +9,17 @@ function askMyName() {
     const h1message = document.getElementById("name").innerText
     alert(h1message + "(แซงซ้ายไม่เคยว่า แต่ถ้าปาดหน้าเจอกัน)");
  }
+
+ const calAge =() => {
+   const yearOfbirth = Number(prompt('กรอกปีเกิด(ค.ศ.)'))
+   const currentlyYear = new Date().getFullYear()
+   const currentlyAge =currentlyYear - yearOfBirth
+   alert(`ยินดีด้วย คุณอยู่ในประเทศนี้มา ${currentlyAge} ปีแล้ว`)
+}
+
+const loadProfile =() => {
+   const name = 'Nawarat' 
+   const province = 'Bangkok' 
+   document.getElementById('my-name').innerHTML = name 
+   document.getElementById('my-province').innerHTML = province
+}
